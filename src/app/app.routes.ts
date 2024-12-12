631e781e68c0ce55b0d1064a025fb08d6553b707
leul tesfu <leultesfu24@gmail.com>
2024-12-12 16:03:45 +0300

    admin-dashboard UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { MainBodyComponent } from './components/landing-page/homepage/main-body.
 import { LoginComponent } from './components/landing-page/login/login.component';
 import { SignupComponent } from './components/landing-page/signup/signup.component';
 import { DocterRoleComponent } from './components/doctor-page/docter-role/docter-role.component';
-import { AdminDashboardComponent } from './components/admin-dashbored/admin-dashbored.component';
 import { PatientRoleComponent } from './components/patient-page/patient-role/patient-role.component';
+import { AdminDashboardComponent } from './components/admin-dashbored/admin-dashbored.component';
 
 export const routes: Routes = [
-  { path: 'admin', component: AdminDashboardComponent },
   { path: '', component: MainBodyComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'doctor', component: DocterRoleComponent },
   { path: 'patient', component: PatientRoleComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 ];
